8345e85a7c0004e3898d03e1457098808bfb0a9e
Karina Serna <kserna9005@gmail.com>
2020-10-09 10:42:11 -0500

    subiendo portafolio

---
+++ assets/config.js
@@ -0,0 +1,45 @@
+'use strict';
+
+// Variables:
+// Json con la metadata de los atributos de las personas:
+const persona = {
+	nombre: "Karina Isabel Serna Hernández",
+	profesion: "Desarrollador Junior",
+	correo: "kserna9005@gmail.com",
+	telefono: "+57 301 3151498",
+	perfil: "Estudiante de Analisís y Desarrollo en Sistemas de Información.",
+	habilidades: {
+		comunicativas:['Asertiva, ','Persuasiva, ','Actitud Positiva, ','Tolerancia, ','Respeto, ','Diálogo Abierto, ','Orden, ','Apoyo'],
+		gestion: ['Liderazgo, ','Evaluación de Proyectos, ','Responsabilidad, ','Estrategias de Gestión, ','Análisis para toma de Decisiones'],
+		tecnicas: ['Sistemas']
+	},
+	hobbies: ['Escuchar Música, ', 'Ver Peliculas, ', 'Ver Series, ', 'Viajar']
+}
+
+// Dejar intácto a partir de aquí.
+// Constantes SENA:
+const banner = "https://3.bp.blogspot.com/-TkVKyVxdxgQ/U2kGFcBl3LI/AAAAAAAABVo/zrDM3ittaz0/s1600/Banner+Soy+Cultura+SENA+-+CTMA.png";
+// Mostramos el objeto persona en la consola:
+console.log(persona);
+
+function dataUser(){
+	// Head titulo:
+	$('#titulo').html(persona.nombre);
+	// Header:
+	$('#nombre').html(persona.nombre);
+	$('#profesion').html(persona.profesion);
+	// Footer:
+	$('#correo').html(persona.correo);
+	$('#telefono').html(persona.telefono);
+	// Sección lateral:
+	$('#banner').html('<img src="'+banner+'" class="img-fluid mt-2" alt="">');
+	$('#perfil').html(persona.perfil);
+	$('#comunicativas').html(persona.habilidades.comunicativas);
+	$('#gestion').html(persona.habilidades.gestion);
+	$('#tecnicas').html(persona.habilidades.tecnicas);
+	// Invocamos una segunda función para traer cada uno de los elementos del arreglo:
+	var aficiones = persona.hobbies.map(function(bar){
+		return '<li class="list-group-item">'+bar+'</li>' 
+	});
+	$('#hobbies').html(aficiones);
+}
